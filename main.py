9b3e9895ce8ed4b98db88d336597ff4afa657197
CBuPenblu <ikulagins@outlook.com>
2024-08-07 01:41:56 +0300

    home

--- main.py
+++ main.py
@@ -1,15 +1,31 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+#@app.route("/")
+#@app.route("/<name>/")
+#def hello_world(name="stranger"):
+#    return f"Hello, {name}"
+
+#@app.route("/")
+#@app.route("/<password>/")
+#def hello_world(password=None):
+#    if password == "1234":
+#        return f"Access granted"
+#    else:
+#        return f"Access denied"
+
+#@app.route("/")
+#def hello_world():
+#    html = """
+#    <h1>Test run of local server</h1>
+#    <p>This is just a text</p>
+#    """
+#    return html
+
 @app.route("/")
 def hello_world():
-    return "Hello World!"
-
-@app.route("/new/")
-@app.route("/newpage/")
-def new():
-    return "new page"
+    return render_template("index.html")
 
 if __name__ == "__main__":
     app.run()
